553b195ef8502b11ce08d4dd35c02a44af83eb9a
iiisong <isaacsong03@gmail.com>
2021-03-01 19:53:23 -0600

    photo gallery

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,12 @@ jQuery(document).ready(function() {
 	
 	"use strict";
 	// Your custom js code goes here.
-
+	$('[class="fancybox"]').fancybox({
+	    buttons : [ 
+	      'download',
+	      'share',
+	      'zoom',
+	      'close',
+	    ]
+	});
 });
